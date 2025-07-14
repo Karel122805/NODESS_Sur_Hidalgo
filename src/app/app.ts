eaaf5d628b4896a78f00e52cb353b4b85c5c8fb3
Adylenne <adymejia808@gmail.com>
2025-07-13 19:36:08 -0600

    Subiendo cambios de actualización Responsiva

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
+  imports: [CommonModule] 
 })
 export class App {
   protected title = 'nodesur';
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenuIfMobile(): void {
+    if (window.innerWidth <= 768) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (window.innerWidth > 768 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 }
